adeb9487fe72eed2dcecfe045b0fe023229ab87d
Renato Groffe <renato.groffe@yahoo.com.br>
2024-10-11 02:01:35 -0300

    Ajustes

--- tests/k6-entraid.js
+++ tests/k6-entraid.js
@@ -1,4 +1,5 @@
 import http from 'k6/http';
+import { check } from 'k6';
 import { sleep } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { jUnit, textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
